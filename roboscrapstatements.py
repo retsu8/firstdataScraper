75f9f59f3c2da195db12ff077cff23235ec6b54b
wjp <wjp@frontlineprocessing.com>
2018-03-06 15:41:25 -0700

    ls

--- roboscrapstatements.py
+++ roboscrapstatements.py
@@ -96,16 +96,16 @@ def cleanhtml(raw_html):
   cleanr = re.compile('<.*?>')
   return re.sub(cleanr, '', raw_html)
 
- def empty_folder(folder=dwn):
-     for the_file in os.listdir(folder):
-         file_path = os.path.join(folder, the_file)
-         try:
-             if os.path.isfile(file_path):
-                 os.unlink(file_path)
-             #elif os.path.isdir(file_path): shutil.rmtree(file_path)
-         except Exception as e:
-             print(e)
-             return False
+ def empty_folder(folder):
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
+            return False
     return True
 
 def manage_firstdata(method):
